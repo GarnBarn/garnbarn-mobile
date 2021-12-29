f1b91e46abd738dcb34d55a290f48e5284d0afa7
Siratee K <sirateek@gmail.com>
2021-12-29 22:51:19 +0700

    Remove the manual firebase initilization and use the flutterfire auto init instead

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:garnbarn_mobile/screens/home_screen.dart';
 import 'package:garnbarn_mobile/services/convert_to_material_color.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const GarnBarn());
 }
 
