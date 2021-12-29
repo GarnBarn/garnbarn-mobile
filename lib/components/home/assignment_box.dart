e0d55c43b9c360c06916d65d733b929664175168
Siratee K <sirateek@gmail.com>
2021-12-29 22:06:49 +0700

    Add pre-implement onclick event for the AssignmentBox

--- lib/components/home/assignment_box.dart
+++ lib/components/home/assignment_box.dart
@@ -4,6 +4,8 @@ import 'package:garnbarn_mobile/services/garnbarn_apis/types/assignment.dart';
 
 class AssignmentBox extends StatelessWidget {
   final Assignment assignment;
+  // Config for Card BorderRadius and Ripple Effect Radius.
+  static final BorderRadius _cardBorderRadius = BorderRadius.circular(20);
   const AssignmentBox({Key? key, required this.assignment}) : super(key: key);
 
   Color getTextColor(Color backgroundColor) {
@@ -22,75 +24,82 @@ class AssignmentBox extends StatelessWidget {
       padding: const EdgeInsets.all(10),
       child: Card(
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20),
+          borderRadius: _cardBorderRadius,
         ),
         color: assignment.getColor(),
-        child: Container(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              // Assignment Name
-              Text(
-                assignment.name,
-                maxLines: 2,
-                style: TextStyle(
-                  fontSize: 25,
-                  overflow: TextOverflow.ellipsis,
-                  fontWeight: FontWeight.w500,
-                  color: getTextColor(
-                    assignment.getColor(),
+        child: InkWell(
+          borderRadius: _cardBorderRadius,
+          onTap: () {
+            // TODO: Implement onTap Event Here
+            print("Hello");
+          },
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                // Assignment Name
+                Text(
+                  assignment.name,
+                  maxLines: 2,
+                  style: TextStyle(
+                    fontSize: 25,
+                    overflow: TextOverflow.ellipsis,
+                    fontWeight: FontWeight.w500,
+                    color: getTextColor(
+                      assignment.getColor(),
+                    ),
                   ),
                 ),
-              ),
-              // Tag Box
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
-                child: assignment.tag != null
-                    ? TagBox(
-                        tag: assignment.tag!,
-                      )
-                    : Container(),
-              ),
-              // Submission Time
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.max,
-                      children: <Widget>[
-                        Icon(
-                          Icons.access_time_filled,
-                          color: getTextColor(
-                            assignment.getColor(),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8),
-                          child: Text(
-                            assignment.dueDate == null
-                                ? "-"
-                                : "${assignment.dueDate!.hour.toString()}:${assignment.dueDate!.minute.toString()}",
-                            style: TextStyle(
-                              fontSize: 17,
-                              color: getTextColor(
-                                assignment.getColor(),
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            softWrap: false,
-                            maxLines: 1,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
+                // Tag Box
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
+                  child: assignment.tag != null
+                      ? TagBox(
+                          tag: assignment.tag!,
+                        )
+                      : Container(),
                 ),
-              ),
-            ],
+                // Submission Time
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Icon(
+                            Icons.access_time_filled,
+                            color: getTextColor(
+                              assignment.getColor(),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8),
+                            child: Text(
+                              assignment.dueDate == null
+                                  ? "-"
+                                  : "${assignment.dueDate!.hour.toString()}:${assignment.dueDate!.minute.toString()}",
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: getTextColor(
+                                  assignment.getColor(),
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                              softWrap: false,
+                              maxLines: 1,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
